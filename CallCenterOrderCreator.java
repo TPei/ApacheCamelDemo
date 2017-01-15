7f9d3eeec4664c50f12b5a49f1f20b51c3002f4e
tpei <t.s.peikert@gmail.com>
2017-01-15 15:14:03 +0100

    initial draft of the call center random order creator component

---
+++ CallCenterOrderCreator.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CallCenterOrderCreator {
+	
+	static String DIRECTORY = "/home/tpei/orders";
+	static String BASE_NAME = "orders_";
+	static int TWO_MINUTES = (120 * 1000);
+
+	public static void main(String[] args) {
+		while(true) {
+			try{
+				// gather all filenames
+				ArrayList<String> results = new ArrayList<String>();
+
+				File[] files = new File(DIRECTORY).listFiles();
+				
+				int maxFileNum = 0;
+
+				for (File file : files) {
+				    if (file.isFile()) {
+				        int num = Integer.parseInt(file.getName().split("_")[1].split("\\.")[0]);
+				        if(num > maxFileNum){
+				        	maxFileNum = num;
+				        }
+				    }
+				}
+				
+				String fileName = BASE_NAME + (maxFileNum + 1) + ".txt";
+			    PrintWriter writer = new PrintWriter("/home/tpei/orders/" + fileName, "UTF-8");
+			    String[] orderStrings = generateRandomOrderString();
+			    for(String orderString : orderStrings){
+			    	writer.write(orderString + "\n");
+			    }
+			    writer.close();
+			    // sleep for two minutes
+				Thread.sleep(TWO_MINUTES);
+			} catch (IOException e) {
+			   // do something
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static String[] generateRandomOrderString() {
+		Random rand = new Random();
+		int orders = rand.nextInt(10); // up to ten orders per file
+		String[] myStringArray = new String[orders];
+		
+		for(int i = 0; i < orders; i++) {
+			int id = rand.nextInt(1000000); // id is some random 7 digit integer
+			int surfs = rand.nextInt(10); // up to 10 surf boards per order
+			int dives = rand.nextInt(10); // up to 10 diving suits per order
+			myStringArray[i] = id + "," + "Some Name," + surfs + "," + dives;
+		}
+		return myStringArray;
+	}
+}
+
